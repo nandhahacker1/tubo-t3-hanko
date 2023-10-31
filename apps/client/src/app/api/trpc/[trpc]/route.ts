aecc0b65f66df8620e3987c2e83290e9b43edfea
Elsa <72644578+nandhahacker1@users.noreply.github.com>
2023-10-31 23:33:06 +0530

    update link in trpc

--- apps/client/src/app/api/trpc/[trpc]/route.ts
+++ apps/client/src/app/api/trpc/[trpc]/route.ts
@@ -1,6 +1,7 @@
 import { appRouter, createContext } from "@repo/server";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { NextApiHandler } from "next";
+import { env } from "@/env";
 
 interface Handler {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -12,6 +13,7 @@ const handler: Handler = async (req, res) => {
   const nextApiHandler: NextApiHandler = createNextApiHandler({
     router: appRouter,
     createContext,
+    endpoint: `${env.SERVER_URL}/api/trpc`,
   });
 
   await nextApiHandler(req, res);
