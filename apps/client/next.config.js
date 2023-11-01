059f9b173efa07bad16792bedba585a3e385c66b
Elsa <72644578+nandhahacker1@users.noreply.github.com>
2023-11-01 12:30:26 +0530

    fix

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,5 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-}
+// @ts-check
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ["@repo/server", "@repo/db"],
+  // We already do linting on GH actions
+  eslint: {
+    ignoreDuringBuilds: !!process.env.CI,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
+};
+
+export default config;
