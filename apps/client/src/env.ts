85254b1918a354d11685522cee7a69cc8b8af705
Elsa <72644578+nandhahacker1@users.noreply.github.com>
2023-11-01 10:44:50 +0530

    change server url

--- apps/client/src/env.ts
+++ apps/client/src/env.ts
@@ -1,5 +1,5 @@
 export const env = {
   NODE_ENV: process.env.NODE_ENV,
   NEXT_PUBLIC_HANKO_API_URL: process.env.NEXT_PUBLIC_HANKO_API_URL || '',
-  SERVER_URL: process.env.SERVER_URL || '',
+  SERVER_URL: "https://tubo-t3-hanko.up.railway.app",
 }
